ee53b1772e81fb59d71658d6d8d7ce30089e9432
guifornagiero <guifornagiero.c@hotmail.com>
2023-10-22 20:35:42 -0300

    feat: criar arquivo de tipos types.c

---
+++ types.c
@@ -0,0 +1,55 @@
+#define TAMANHO_NOME 100
+#define TAMANHO_RG 9
+
+typedef struct Data
+{
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+typedef struct Registro
+{
+    char nome[TAMANHO_NOME];
+    int idade;
+    char RG[TAMANHO_RG];
+    Data entrada;
+} Registro;
+
+typedef struct ElementoLista
+{
+    Registro dados;
+    struct ElementoLista *proximo;
+} ElementoLista;
+
+typedef struct Lista
+{
+    ElementoLista inicio;
+    int quantidade;
+} Lista;
+
+typedef struct ElementoFila
+{
+    Registro dados;
+    struct ElementoFila *proximo;
+} ElementoFila;
+
+typedef struct Fila
+{
+    ElementoFila head;
+    ElementoFila tail;
+    int quantidade;
+} Fila;
+
+typedef struct ElementoArvoreBinaria
+{
+    Registro dados;
+    struct ElementoArvoreBinaria *filho_esquerdo;
+    struct ElementoArvoreBinaria *filho_direito;
+} ElementoArvoreBinaria;
+
+typedef struct ArvoreBinaria
+{
+    ElementoArvoreBinaria raiz;
+    int quantidade;
+} ArvoreBinaria;
