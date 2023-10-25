541fedf1367a13198627fd67748d4e7bc3a82e07
guifornagiero <guifornagiero.c@hotmail.com>
2023-10-25 16:13:57 -0300

    feat: criada lógica de cadastro de usuário, inserção na lista e print

--- main.c
+++ main.c
@@ -8,7 +8,25 @@ void limpaConsole()
     system("cls");
 }
 
-void menuPrincipal()
+Lista *iniciaLista()
+{
+    Lista *lista = malloc(sizeof(Lista));
+    lista->inicio = NULL;
+    lista->quantidade = 0;
+
+    return lista;
+}
+
+ElementoLista *iniciaElementoLista(Registro paciente)
+{
+    ElementoLista *novo = malloc(sizeof(ElementoLista));
+    novo->dados = paciente;
+    novo->proximo = NULL;
+
+    return novo;
+}
+
+int menuPrincipal()
 {
     printf("\nPROJETO - ESTRUTURA DE DADOS\n");
     printf("- Aplicativo de gerenciamento de servico de saude -\n\n");
@@ -18,24 +36,8 @@ void menuPrincipal()
     printf("3 - Pesquisa\n");
     printf("4 - Carregar / Salvar\n");
     printf("5 - Sobre\n\n");
-}
 
-void cadastro()
-{
-    limpaConsole();
-
-    printf("\n\nOPCAO 1 - CADASTRO\n\n");
-    printf("1 - Cadastrar novo paciente\n");
-    printf("2 - Consultar paciente cadastrado\n");
-    printf("3 - Mostrar lista completa\n");
-    printf("4 - Atualizar dados de paciente\n");
-    printf("5 - Remover paciente\n");
-}
-
-int main()
-{
-    int escolha = 1;
-    menuPrincipal();
+    int escolha = 0;
 
     do
     {
@@ -43,12 +45,119 @@ int main()
         scanf("%d", &escolha);
     } while (escolha < 1 || escolha > 5);
 
-    printf("escolha: %d", escolha);
+    return escolha;
+}
+
+void cadastrarNovoPaciente(Lista *lista, Registro paciente)
+{
+    ElementoLista *novo = iniciaElementoLista(paciente);
+
+    if (lista->inicio == NULL)
+        lista->inicio = novo;
+    else
+    {
+        novo->proximo = lista->inicio;
+        lista->inicio = novo;
+    }
+
+    lista->quantidade++;
+}
+
+void mostrarListaCompleta(Lista *lista)
+{
+    ElementoLista *atual = lista->inicio;
+
+    while (atual != NULL)
+    {
+        printf("\nNome -> %s\n", atual->dados.nome);
+        printf("Idade -> %d\n", atual->dados.idade);
+        printf("RG -> %s\n", atual->dados.RG);
+        printf("Entrada -> %d/%d/%d\n", atual->dados.entrada.dia, atual->dados.entrada.mes, atual->dados.entrada.ano);
+        atual = atual->proximo;
+    }
+    printf("\nQuantidade -> %d", lista->quantidade);
+}
+
+void cadastro(Lista *lista)
+{
+    limpaConsole();
+
+    printf("OPCAO 1 - CADASTRO\n\n");
+    printf("1 - Cadastrar novo paciente\n");
+    printf("2 - Consultar paciente cadastrado\n");
+    printf("3 - Mostrar lista completa\n");
+    printf("4 - Atualizar dados de paciente\n");
+    printf("5 - Remover paciente\n\n");
+
+    int escolha = 0;
+
+    do
+    {
+        printf("Digite a opcao escolhida: ");
+        scanf("%d", &escolha);
+    } while (escolha < 1 || escolha > 5);
+
+    if (escolha == 1)
+    {
+        char nome[TAMANHO_NOME];
+        int idade;
+        char RG[TAMANHO_RG];
+        char entradaData[12];
+
+        Registro novoPaciente;
+
+        limpaConsole();
+
+        printf("OPCAO 1 - CADASTRAR NOVO PACIENTE\n\n");
+        printf("Nome: ");
+        getchar();
+        fgets(nome, sizeof(nome), stdin);
+        nome[strcspn(nome, "\n")] = '\0';
+        strcpy(novoPaciente.nome, nome);
+
+        printf("Idade: ");
+        scanf("%d", &idade);
+        novoPaciente.idade = idade;
+
+        printf("RG: ");
+        getchar();
+        fgets(RG, sizeof(RG), stdin);
+        RG[strcspn(RG, "\n")] = '\0';
+        strcpy(novoPaciente.RG, RG);
+
+        int dia, mes, ano;
+
+        printf("Digite a data (dd/mm/AAAA): ");
+        fgets(entradaData, sizeof(entradaData), stdin);
+
+        sscanf(entradaData, "%d/%d/%d", &dia, &mes, &ano);
+
+        novoPaciente.entrada.dia = dia;
+        novoPaciente.entrada.mes = mes;
+        novoPaciente.entrada.ano = ano;
+
+        cadastrarNovoPaciente(lista, novoPaciente);
+
+        strcpy(novoPaciente.nome, "aaaaa");
+        cadastrarNovoPaciente(lista, novoPaciente);
+
+        strcpy(novoPaciente.nome, "bbbbbbb");
+        cadastrarNovoPaciente(lista, novoPaciente);
+
+        mostrarListaCompleta(lista);
+    }
+}
+
+int main()
+{
+    Lista *lista = iniciaLista();
+
+    int escolha = menuPrincipal();
 
     switch (escolha)
     {
     case 1:
-        cadastro();
+        cadastro(lista);
         break;
     }
 
