a19d2aa486f0b64ab209a645ccc72b8c9e648bc8
guifornagiero <guifornagiero.c@hotmail.com>
2023-11-06 21:05:20 -0300

    feat: criar método de atendimento e funções auxiliares

--- main.c
+++ main.c
@@ -26,6 +26,42 @@ ElementoLista *iniciaElementoLista(Registro paciente)
     return novo;
 }
 
+ElementoLista *buscaPacientePorRG(Lista *lista, char *RG)
+{
+    ElementoLista *atual = lista->inicio;
+
+    while (atual != NULL)
+    {
+        if (strcmp(atual->dados.RG, RG) == 0)
+        {
+            return atual;
+        }
+        atual = atual->proximo;
+    }
+
+    return NULL;
+}
+
+Fila *iniciaFila()
+{
+    Fila *fila = malloc(sizeof(Fila));
+    fila->head = NULL;
+    fila->tail = NULL;
+    fila->quantidade = 0;
+
+    return fila;
+}
+
+ElementoFila *iniciaElementoFila(Registro paciente)
+{
+    ElementoFila *novo = malloc(sizeof(ElementoFila));
+    novo->dados = paciente;
+    novo->proximo = NULL;
+    novo->anterior = NULL;
+
+    return novo;
+}
+
 int menuPrincipal()
 {
     printf("\nPROJETO - ESTRUTURA DE DADOS\n");
@@ -50,6 +86,7 @@ int menuPrincipal()
 }
 
 #pragma region CADASTRO (OPCAO 1)
+
 void cadastrarNovoPaciente(Lista *lista)
 {
     char nome[TAMANHO_NOME];
@@ -102,22 +139,6 @@ void cadastrarNovoPaciente(Lista *lista)
     }
 
     lista->quantidade++;
-}
-
-ElementoLista *buscaPacientePorRG(Lista *lista, char *RG)
-{
-    ElementoLista *atual = lista->inicio;
-
-    while (atual != NULL)
-    {
-        if (strcmp(atual->dados.RG, RG) == 0)
-        {
-            return atual;
-        }
-        atual = atual->proximo;
-    }
-
-    return NULL;
 }
 
 void consultarPacienteCadastrado(Lista *lista)
@@ -264,8 +285,6 @@ void removerPaciente(Lista *lista)
     }
 }
 
-#pragma endregion
-
 void cadastro(Lista *lista)
 {
     limpaConsole();
@@ -274,7 +293,6 @@ void cadastro(Lista *lista)
 
     while (sair != 1)
     {
-
         printf("OPCAO 1 - CADASTRO\n\n");
         printf("1 - Cadastrar novo paciente\n");
         printf("2 - Consultar paciente cadastrado\n");
@@ -291,26 +309,159 @@ void cadastro(Lista *lista)
             scanf("%d", &escolha);
         } while (escolha < 0 || escolha > 5);
 
-        if (escolha == 1)
+        switch (escolha)
+        {
+        case 1:
             cadastrarNovoPaciente(lista);
-        if (escolha == 2)
+            break;
+        case 2:
             consultarPacienteCadastrado(lista);
-        if (escolha == 3)
+            break;
+        case 3:
             mostrarListaCompleta(lista);
-        if (escolha == 4)
+            break;
+        case 4:
             atualizarDadosPaciente(lista);
-        if (escolha == 5)
+            break;
+        case 5:
             removerPaciente(lista);
-        if (escolha == 0)
+            break;
+        case 0:
             sair = 1;
+            break;
+        default:
+            break;
+        }
     }
 }
+
+#pragma endregion
+
+#pragma region ATENDIMENTO (OPCAO 2)
+
+void enfileirarPaciente(Lista *lista, Fila *fila)
+{
+    limpaConsole();
+
+    char RG[TAMANHO_RG];
+
+    printf("OPCAO 1 - ENFILEIRAR PACIENTE\n\n");
+    printf("Digite o RG do paciente: ");
+    scanf("%s", RG);
+
+    ElementoLista *paciente = buscaPacientePorRG(lista, RG);
+
+    if (paciente != NULL)
+    {
+        ElementoFila *novoPaciente = iniciaElementoFila(paciente->dados);
+
+        if (fila->quantidade == 0)
+            fila->head = novoPaciente;
+        else
+        {
+            fila->tail->proximo = novoPaciente;
+            novoPaciente->anterior = fila->tail;
+        }
+
+        fila->tail = novoPaciente;
+        fila->quantidade++;
+
+        printf("\n- Paciente com RG %s entrou para a fila de atendimento! -\n\n", RG);
+    }
+    else
+        printf("\nPaciente com RG %s nao encontrado.\n\n", RG);
+}
+
+void desenfileirarPaciente(Fila *fila)
+{
+    if (fila->quantidade != 0)
+    {
+        ElementoFila *paciente = fila->head;
+        fila->head = paciente->proximo;
+        fila->quantidade--;
+
+        printf("\n- Paciente com RG %s atendido! -\n\n", paciente->dados.RG);
+
+        free(paciente);
+    }
+    else
+        printf("A fila de pacientes esta vazia. Nenhum paciente para ser atendido!\n\n");
+}
+
+void mostrarFila(Fila *fila)
+{
+    limpaConsole();
+
+    printf("FILA DE PACIENTES COMPLETA\n\n");
+
+    if (fila->quantidade != 0)
+    {
+        ElementoFila *atual = fila->head;
+
+        while (atual != NULL)
+        {
+            printf("Nome: %s\n", atual->dados.nome);
+            printf("RG: %s\n", atual->dados.RG);
+            printf("Idade: %d\n\n", atual->dados.idade);
+
+            atual = atual->proximo;
+        }
+
+        printf("Quantidade -> %d\n", fila->quantidade);
+        printf("-------------------------------------------------------\n\n");
+    }
+    else
+        printf("Fila de pacientes vazia!\n\n");
+}
+
+void atendimento(Lista *lista, Fila *fila)
+{
+    limpaConsole();
+
+    int sair = 0;
+
+    while (sair != 1)
+    {
+        printf("OPCAO 2 - ATENDIMENTO\n\n");
+        printf("1 - Enfileirar paciente\n");
+        printf("2 - Desenfileirar paciente\n");
+        printf("3 - Mostrar fila\n");
+        printf("0 - Sair de atendimento\n\n");
+
+        int escolha = 0;
+
+        do
+        {
+            printf("Digite a opcao escolhida: ");
+            scanf("%d", &escolha);
+        } while (escolha < 0 || escolha > 3);
+
+        switch (escolha)
+        {
+        case 1:
+            enfileirarPaciente(lista, fila);
+            break;
+        case 2:
+            desenfileirarPaciente(fila);
+            break;
+        case 3:
+            mostrarFila(fila);
+            break;
+        case 0:
+            sair = 1;
+            break;
+        }
+    }
+}
+
+#pragma endregion
 
 int main()
 {
     int sair = 0;
 
     Lista *lista = iniciaLista();
+    Fila *fila = iniciaFila();
 
     while (sair != 1)
     {
@@ -322,6 +473,9 @@ int main()
         {
         case 1:
             cadastro(lista);
+            break;
+        case 2:
+            atendimento(lista, fila);
             break;
         case 0:
             sair = 1;
